482630de4cd17bb3a4506756555f90c284e7eddd
Brendan <bc@projectn.co>
2020-11-16 11:35:59 -0800

    feat(SUP-1866): Python Sdk Poc
    - Implements basic request signing/mutation

--- bolt.py
+++ bolt.py
@@ -2,7 +2,7 @@
 Simple wrapper of boto3 client.
 Bolt will do some special stuff in the background to ensure that:
 - Signing is disabled
-- Addressing style is == to path
+?## - Addressing style is == to path
 
 - Implement a hook which intercepts request ()
     cli.register_last('before-send.s3', Bolt.send)
@@ -19,60 +19,52 @@ from urllib.parse import urlsplit, urlunsplit
 
 import boto3
 from boto3 import Session
-from botocore import UNSIGNED
-from botocore.config import Config
-from botocore.session import get_session
 from botocore.handlers import disable_signing
-
-import urllib.parse as urlparse
-from urllib.parse import parse_qs
-
-def _inject_header(*args, **kwargs):
-  print('\n=======debug inject enter=========\n')
-  bolt_url='https://bolt.turing-aws-gxxq.bolt.projectn.co'  # Remove Hardcoded Bolt URL
-  scheme, host, _, _, _ = urlsplit(bolt_url)
-
-  prepared_request = kwargs['request']
-  print(prepared_request)
-  _, _, path, query, fragment = urlsplit(prepared_request.url)
-  prepared_request.url = urlunsplit((scheme, host, path, query, fragment))
-  print('\n')
-  print(prepared_request)
-
-  # Sets the X-Bolt-Identity-Request header on the outbound request.
-  # This will allow the Bolt service to proxy to AWS to resolve the caller's identity to check against ACLs.
-  url = prepared_request.headers['X-Bolt-Identity-Request'] = Session(profile_name='nobolt').client('sts').generate_presigned_url(
-      'get_caller_identity', ExpiresIn=10)
-
-  print('\n')
-  print(prepared_request)
-  #TODO: avoid making sts calls each time in the same session
-
-  print('\n=======inject exit=========\n')
+from botocore.auth import SigV4Auth
+from botocore.awsrequest import AWSRequest
+from botocore.session import get_session
 
 def client(*args, **kwargs):
-  # If client specifies 's3' service then intercept headers
+  # If client specifies 's3' service then mutate request for Bolt.
+  # Otherwise, return an unmodified client.
+
   if kwargs.get('session_name') == 's3' or 's3' in args:
-    session = Session(profile_name='bolt')
-    session.events.register_last('before-send.s3', _inject_header)
+    session = Session(profile_name='nobolt')
+    creds = session.get_credentials()
+
+    # Use inner function to curry 'creds' into callback
+    def inject_header(*args, **kwargs):
+      bolt_url='https://bolt.turing-aws-gxxq.bolt.projectn.co'  # Remove Hardcoded Bolt URL
+
+      # Modify request URL to redirect to bolt
+      scheme, host, _, _, _ = urlsplit(bolt_url)
+      prepared_request = kwargs['request']
+      _, _, path, query, fragment = urlsplit(prepared_request.url)
+      prepared_request.url = urlunsplit((scheme, host, path, query, fragment))
+
+      # Sign a get-caller-identity request
+      REGION='us-east-2' # Get session region
+      method='GET'
+      url = 'https://sts.amazonaws.com/'
+      data = 'Action=GetCallerIdentity&Version=2011-06-15'
+      request = AWSRequest(method=method, url=url, data=data, params=None, headers=None)
+      SigV4Auth(creds, "sts", REGION).add_auth(request)
+
+      # Pass signed STS headers into Bolt request
+      # Needs: "X-Amz-Security-Token"
+      for key in ["X-Amz-Date", "Authorization"]:
+        prepared_request.headers[key] = request.headers[key]
+
+    session.events.register_last('before-send.s3', inject_header)
     session.events.register(
       'choose-signer', disable_signing, unique_id='bolt-disable-signing')
 
-    return session.client('s3', config=Config(signature_version=UNSIGNED)) # 'UNSIGNED' may be redundant
+    return session.client('s3')
 
-    # Modifies config if passed in by user. Else, adds config to kwargs
-    '''
-    config = kwargs.get('config')
-    if config != None: 
-      setattr(config, 'signature_version', UNSIGNED) 
-    else:
-      kwargs['config'] = Config(signature_version=UNSIGNED)
-    return session.client(*args, **kwargs)
-    '''
   else:
     return boto3.client(*args, **kwargs)
 
+
+
 s3 = client('s3')
 print(s3.list_buckets())
-
-# TODO: Support session class creation...
