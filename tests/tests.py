59355539f938ac381199fbb4c3e7f9e5e1d83a9d
Brendan <bc@projectn.co>
2020-11-19 18:30:28 -0800

    feat(SUP-1886): Python SDK Version 1
    - readme + packaging support
    - implements identical interface to boto3
    - supports Bolt URL through ENV variable and by explicit pass to .client
    method
    - error handling and hidden imports

---
+++ tests/tests.py
@@ -0,0 +1,50 @@
+import boto3
+import bolt as bolt3
+
+def client_tests():
+  # Test signing for s3
+  s3_boto = boto3.client('s3')
+  s3_bolt = bolt3.client('s3')
+
+  lboto = s3_boto.list_buckets()
+  lbolt = s3_bolt.list_buckets()
+
+  assert(lboto != lbolt)
+
+  # Test that other services remain un-affected
+  route_boto = boto3.client('route53')
+  route_bolt = bolt3.client('route53')
+
+  aboto = route_boto.get_account_limit(
+    Type='MAX_HEALTH_CHECKS_BY_OWNER',
+  )
+  abolt = route_bolt.get_account_limit(
+    Type='MAX_HEALTH_CHECKS_BY_OWNER',
+  )
+
+def session_tests():
+  # Test signing for s3
+  session_boto = boto3.Session(profile_name='nobolt')
+  session_bolt = bolt3.Session(profile_name='nobolt')
+
+  s3_boto = session_boto.client('s3')
+  s3_bolt = session_bolt.client('s3')
+
+  lboto = s3_boto.list_buckets()
+  lbolt = s3_bolt.list_buckets()
+
+  assert(lboto != lbolt)
+
+  # Test that other services remain un-affected
+  route_boto = session_boto.client('route53')
+  route_bolt = session_bolt.client('route53')
+
+  aboto = route_boto.get_account_limit(
+    Type='MAX_HEALTH_CHECKS_BY_OWNER',
+  )
+  abolt = route_bolt.get_account_limit(
+    Type='MAX_HEALTH_CHECKS_BY_OWNER',
+  )
+
+client_tests()
+session_tests()
