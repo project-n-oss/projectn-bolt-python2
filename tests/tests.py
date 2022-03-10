f2ae015f048a64012a4be40d3240fbccee58e95f
Mack Yi <my@projectn.co>
2022-03-10 11:09:59 -0800

    feat: support new Bolt SSL setup

--- tests/tests.py
+++ tests/tests.py
@@ -10,6 +10,7 @@ def client_tests():
     lboto = s3_boto.list_buckets()
     lbolt = s3_bolt.list_buckets()
 
+    print(lbolt)
     assert (lboto != lbolt)
 
     # Test that other services remain un-affected
@@ -31,8 +32,8 @@ def test_refresh():
 
 def session_tests():
     # Test signing for s3
-    session_boto = boto3.Session(profile_name='nobolt')
-    session_bolt = bolt3.Session(profile_name='nobolt')
+    session_boto = boto3.Session()
+    session_bolt = bolt3.Session()
 
     s3_boto = session_boto.client('s3')
     s3_bolt = session_bolt.client('s3')
@@ -40,6 +41,7 @@ def session_tests():
     lboto = s3_boto.list_buckets()
     lbolt = s3_bolt.list_buckets()
 
+    print(lbolt)
     assert (lboto != lbolt)
 
     # Test that other services remain un-affected
